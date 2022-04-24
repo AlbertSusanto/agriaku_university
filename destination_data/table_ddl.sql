464995cb65bfd949167b5f1290de7683d53e6a02
AlbertSusanto <alberts.jonathans@gmail.com>
2022-04-25 00:29:43 +0800

    add data mart ddl

---
+++ destination_data/table_ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE weekly_course_attendance(
+    schedule_start_date DATE,
+    schedule_end_date DATE,
+    academic_year INT,
+    semester INT,
+    course_days VARCHAR,
+    weekly_attendance_percentage INT
+)
